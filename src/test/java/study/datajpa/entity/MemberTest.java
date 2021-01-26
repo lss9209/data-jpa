09f34af666887f0df91cca799f1de4bb8be8e8fb
sangseunglee <lss9209>
2021-01-26 20:33:22 +0900

    Spring Data JPA Practice - Day 1

---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,26 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        
+    }
+
+}
