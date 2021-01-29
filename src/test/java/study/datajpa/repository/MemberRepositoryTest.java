d242db7d47114825bfe1a725a48736299cebd210
sangseunglee <lss9209>
2021-01-29 20:39:14 +0900

    Project of Spring Data JPA Day 4

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,12 +3,17 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,6 +26,8 @@ class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
     @Autowired TeamRepository teamRepository;
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     public void testMember() {
@@ -157,20 +164,70 @@ class MemberRepositoryTest {
         memberRepository.save(new Member("member5",10));
 
         int age = 10;
-        int offset = 1;
-        int limit = 3;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
 
         //when
-        //List<Member> members = memberRepository.findByPage(age, offset, limit);
-        //long totalCount = memberRepository.totalCount(age);
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
 
-        //페이지 계산 공식 적용...
-        // totalPage = totalCount / size ...
-        // 마지막 페이지 ...
-        // 최초 페이지 ...
+        Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
 
         //then
-        //assertThat(members.size()).isEqualTo(3);
-        //assertThat(totalCount).isEqualTo(5);
+        List<Member> content = page.getContent();
+        long totalElements = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
     }
+
+    @Test
+    public void bulkUpdate() {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+
+        List<Member> result = memberRepository.findByUsername("member5");
+        Member member5 = result.get(0);
+        System.out.println("member5 = " + member5);
+
+
+        //then
+        assertThat(resultCount).isEqualTo(3);
+    }
+
+     @Test
+    public void findMemberLazy() {
+        //given
+         //member1 -> teamA
+         //member2 -> teamB
+
+         Team teamA = new Team("teamA");
+         Team teamB = new Team("teamB");
+         teamRepository.save(teamA);
+         teamRepository.save(teamB);
+         Member member1 = new Member("member1", 10, teamA);
+         Member member2 = new Member("member2", 20, teamB);
+         memberRepository.save(member1);
+         memberRepository.save(member2);
+
+         em.flush();
+         em.clear();
+
+         //when
+         List<Member> members = memberRepository.findAll();
+
+         for (Member member : members) {
+             System.out.println("member = " + member.getUsername());
+             System.out.println("member.team = " + member.getTeam().getName());
+         }
+     }
 }
